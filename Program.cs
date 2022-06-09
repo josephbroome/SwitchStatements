16ecda6fe5c1d4d4e139a424721ce48b8ec10f97
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-09 16:53:51 -0700

    updates

--- Program.cs
+++ Program.cs
@@ -4,119 +4,162 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, what is your name?");
-        var userName = Console.ReadLine();
+      //  Console.WriteLine("Hello, what is your name?");
+      //  var userName = Console.ReadLine();
 
-        Console.WriteLine($"Hello {userName}, what is your favorite subject?");
+      //  Console.WriteLine($"Hello {userName}, what is your favorite subject?");
 
-        var subject = Console.ReadLine();
+      //  var subject = Console.ReadLine();
 
-        switch (subject)
+      //  switch (subject)
+      //  {
+      //      case "History":
+      //          Console.WriteLine("Wow History is my favorite subject to!");
+      //          break;
+
+      //      case "Math":
+      //          Console.WriteLine("Math is very hard, I am surprised its your favorite subject since you are human");
+      //          break;
+
+      //      case "Science":
+      //          Console.WriteLine("My uncle likes Science as well, lets do experiments!!");
+      //          break;
+
+      //      case "English":
+      //          Console.WriteLine("I dont like English, but I am happy you do");
+      //          break;
+
+      //      case "Chemistry":
+      //          Console.WriteLine("Chemistry is fun, lets make a valcano");
+      //          break;
+
+      //      default:
+      //          Console.WriteLine($"I have never heard of that subject before, {subject} sounds neat!");
+      //          break;
+
+
+
+
+
+
+
+      //  }
+
+
+
+
+
+
+
+
+      //  var favoriteNumber = 200;
+
+      //  var userGuess = int.Parse(Console.ReadLine());
+
+      //  if (userGuess < favoriteNumber)
+      //  {
+      //      Console.WriteLine("to low");
+
+      //  }
+      //  else if (userGuess > favoriteNumber)
+      //  {
+      //      Console.WriteLine("to high");
+      //  }
+
+      //  else
+      //  {
+      //      Console.WriteLine("you got it");
+      //  }
+
+
+      //  int a = 5;
+      //  a++;
+      //  int b = a;
+      //  b++;
+      //  int c = b;
+
+      //  Console.WriteLine($"{c}");
+      //  Console.WriteLine($"{c}");
+
+      // int e = 17;
+      // int f = 4;
+      // int g = e / f;
+      //  var remainder = e % f; 
+
+      //  Console.WriteLine($" {e}/ {f} equals {g}, with a remainder of {remainder}");
+      //  int i = int.Parse(Console.ReadLine());
+      //  var positiveMessage = (i > 0) ? "you are postive!" : "you are not positve";
+
+      //  Console.WriteLine($"{positiveMessage}");
+
+      //  int h = 1;
+      //  int j = 3; 
+
+      //  if (h <=1 && j <=2)
+      //  {
+      //      Console.WriteLine("Hello");
+      //  }
+      //  else if (j <=3 && h >= 2)
+      //  {
+      //      Console.WriteLine("Goodbye");
+      //  }
+      //  else
+      //  {
+      //      Console.WriteLine("poop");
+      //  }
+      //  int k = 5;
+      //  int l = 6;
+
+
+      //Console.WriteLine( k += l);
+
+
+      //  FavoriteSubject();
+      //  IsnumberOdd(11);
+
+
+      //  Console.WriteLine("true or false the state capitol of Califronia is Sacramento?");
+
+      //  var stateCapitol = Console.ReadLine();
+
+      //  switch (stateCapitol)
+      //  {
+      //      case "true":
+      //          Console.WriteLine("Correct");
+      //          break;
+      //      case "false":
+      //          Console.WriteLine("you got it wrong dummy!");
+      //          break;
+
+                    
+      //  }
+      //  var z = int.Parse(Console.ReadLine());
+
+        string teacher;
+       int w;
+        w = 5;
+
+        Console.WriteLine($"{w}");
+
+        double milesRanToday = 0;
+        Console.WriteLine($"How many miles did you run today?");
+
+        var response=Console.ReadLine();
+
+        bool isParsable=double.TryParse(response, out milesRanToday);
+        
+        if (isParsable==true)
         {
-            case "History":
-                Console.WriteLine("Wow History is my favorite subject to!");
-                break;
-
-            case "Math":
-                Console.WriteLine("Math is very hard, I am surprised its your favorite subject since you are human");
-                break;
-
-            case "Science":
-                Console.WriteLine("My uncle likes Science as well, lets do experiments!!");
-                break;
-
-            case "English":
-                Console.WriteLine("I dont like English, but I am happy you do");
-                break;
-
-            case "Chemistry":
-                Console.WriteLine("Chemistry is fun, lets make a valcano");
-                break;
-
-            default:
-                Console.WriteLine($"I have never heard of that subject before, {subject} sounds neat!");
-                break;
-
-
-
-
-
-
+            Console.WriteLine($" you ran {milesRanToday}");
 
         }
-
-
-
-
-
-
-
-
-        var favoriteNumber = 200;
-
-        var userGuess = int.Parse(Console.ReadLine());
-
-        if (userGuess < favoriteNumber)
-        {
-            Console.WriteLine("to low");
-
-        }
-        else if (userGuess > favoriteNumber)
-        {
-            Console.WriteLine("to high");
-        }
-
         else
         {
-            Console.WriteLine("you got it");
+            Console.WriteLine("Please type a number next time.");
         }
-
-
-        int a = 5;
-        a++;
-        int b = a;
-        b++;
-        int c = b;
-
-        Console.WriteLine($"{c}");
-        Console.WriteLine($"{c}");
-
-       int e = 17;
-       int f = 4;
-       int g = e / f;
-        var remainder = e % f; 
-
-        Console.WriteLine($" {e}/ {f} equals {g}, with a remainder of {remainder}");
-        int i = int.Parse(Console.ReadLine());
-        var positiveMessage = (i > 0) ? "you are postive!" : "you are not positve";
-
-        Console.WriteLine($"{positiveMessage}");
-
-        int h = 1;
-        int j = 3; 
-
-        if (h <=1 && j <=2)
-        {
-            Console.WriteLine("Hello");
-        }
-        else if (j <=3 && h >= 2)
-        {
-            Console.WriteLine("Goodbye");
-        }
-        else
-        {
-            Console.WriteLine("poop");
-        }
-        int k = 5;
-        int l = 6;
-
-
-      Console.WriteLine( k += l);
-
-
-        FavoriteSubject();
-        IsnumberOdd(11);
+      var answer = (isParsable == true) ? $"You ran {milesRanToday} " : "Plese type a number next time";
     
+      Console.WriteLine(answer);
     }
 
 
