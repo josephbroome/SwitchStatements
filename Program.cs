9687dbf0ba5bfd30023a51e8dc17f6ee4fab5325
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-05 07:18:26 -0700

    updates

--- Program.cs
+++ Program.cs
@@ -4,43 +4,43 @@ class Program
 {
     static void Main(string[]args)
     {
-        //Console.WriteLine("Hello, what is your name?");
-        //var userName= Console.ReadLine();
-        
-        //Console.WriteLine($"Hello {userName}, what is your favorite subject?");
+        Console.WriteLine("Hello, what is your name?");
+        var userName = Console.ReadLine();
 
-        //var subject= Console.ReadLine();
+        Console.WriteLine($"Hello {userName}, what is your favorite subject?");
 
-        //switch (subject)
-        //{
-        //    case "History":
-        //        Console.WriteLine("Wow History is my favorite subject to!");
-        //        break;
+        var subject = Console.ReadLine();
 
-        //    case "Math":
-        //        Console.WriteLine("Math is very hard, I am surprised its your favorite subject since you are human");
-        //        break ;
+        switch (subject)
+        {
+            case "History":
+                Console.WriteLine("Wow History is my favorite subject to!");
+                break;
 
-        //    case "Science":
-        //        Console.WriteLine("My uncle likes Science as well, lets do experiments!!");
-        //        break;
+            case "Math":
+                Console.WriteLine("Math is very hard, I am surprised its your favorite subject since you are human");
+                break;
 
-        //    case "English":
-        //        Console.WriteLine("I dont like English, but I am happy you do");
-        //        break;
+            case "Science":
+                Console.WriteLine("My uncle likes Science as well, lets do experiments!!");
+                break;
 
-        //    case "Chemistry":
-        //        Console.WriteLine("Chemistry is fun, lets make a valcano");
-        //        break;
-            
-        //    default:
-        //        Console.WriteLine($"I have never heard of that subject before, {subject} sounds neat!");
-        //        break;
+            case "English":
+                Console.WriteLine("I dont like English, but I am happy you do");
+                break;
 
-  
-        
-        
-        //}
+            case "Chemistry":
+                Console.WriteLine("Chemistry is fun, lets make a valcano");
+                break;
+
+            default:
+                Console.WriteLine($"I have never heard of that subject before, {subject} sounds neat!");
+                break;
+
+
+
+
+        }
 
 
 
@@ -78,7 +78,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed correctly");
+                Console.WriteLine("You guessed correctly!!");
             
                 isRight=true;
             }
